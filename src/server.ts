ccac60cddc2283deda67dfb909eb5db040baca26
Garcez17 <ggarcez613@gmail.com>
2020-10-26 17:44:07 -0300

    Add Eslint, prettier, editorconfig, babel and jest

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', async (req, res) => res.json({ message: 'Hello'}));
+app.get('/', async (req, res) => res.json({ message: 'Hello' }));
 
 app.listen(3333);
