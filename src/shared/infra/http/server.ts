ba5dd8c98b4774139d9a251b987d4f08573b4040
Garcez17 <ggarcez613@gmail.com>
2020-10-27 12:17:11 -0300

    feat: add typeorm and start to use DDD
    add typeorm configurations and create the structure of DDD on the application

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import '@shared/infra/typeorm';
+
 const app = express();
 
 app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
