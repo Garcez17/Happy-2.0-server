c83955be1fa61166a6db2325b01abc43d7b02072
Garcez17 <ggarcez613@gmail.com>
2020-10-27 23:14:31 -0300

    feat: add create user
    Add create user feature, using bcrypt to encrypt the password, class-transformer to remove password
    from route response and tsyringe for dependency injection.

---
+++ src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -0,0 +1,10 @@
+import { hash } from 'bcryptjs';
+import IHashProvider from '../models/IHashProvider';
+
+class BCryptHashProvider implements IHashProvider {
+  public async generateHash(payload: string): Promise<string> {
+    return hash(payload, 8);
+  }
+}
+
+export default BCryptHashProvider;
