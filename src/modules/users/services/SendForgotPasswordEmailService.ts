6dea1551760d4665e6108aaf12bf998eeee49278
Garcez17 <ggarcez613@gmail.com>
2020-11-07 10:01:53 -0300

    feat: add forgot password email template

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,10 +1,10 @@
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
-// import User from '../infra/typeorm/entities/User';
 
 interface IRequest {
   email: string;
@@ -30,6 +30,13 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -37,10 +44,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[Happy] Recuperação de senha',
       templateData: {
-        template: 'Olá, {{name}}! Recuperação de senha solicitada: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
