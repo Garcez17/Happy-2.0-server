d7d835ffd425d578c418d5db7d1efc4a6fd5846a
Garcez17 <ggarcez613@gmail.com>
2020-11-07 18:58:03 -0300

    feat: add validation in routes using celebrate

--- src/modules/orphanages/infra/http/routes/orphanages.routes.ts
+++ src/modules/orphanages/infra/http/routes/orphanages.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { celebrate, Joi, Segments } from 'celebrate';
+
 import uploadConfig from '@config/upload';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
@@ -17,13 +19,62 @@ const orphanagesVailablesController = new OrphanagesVailablesController();
 const orphanageNotVailablesController = new OrphanageNotVailablesController();
 
 orphanagesRouter.get('/vailables', orphanagesVailablesController.index);
-orphanagesRouter.get('/not-vailables', orphanageNotVailablesController.index);
+orphanagesRouter.get(
+  '/not-vailables',
+  ensureAuthenticated,
+  orphanageNotVailablesController.index,
+);
 
-orphanagesRouter.post('/', upload.array('images'), orphanagesController.create);
+orphanagesRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      about: Joi.string().required(),
+      instructions: Joi.string().required(),
+      opening_hours: Joi.string().required(),
+      open_on_weekends: Joi.boolean().required(),
+    },
+  }),
+  upload.array('images'),
+  orphanagesController.create,
+);
 
-orphanagesRouter.put('/:id', orphanagesController.update);
+orphanagesRouter.put(
+  '/:id',
+  ensureAuthenticated,
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      about: Joi.string().required(),
+      instructions: Joi.string().required(),
+      opening_hours: Joi.string().required(),
+      open_on_weekends: Joi.boolean().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  orphanagesController.update,
+);
 
-orphanagesRouter.patch('/:id', orphanageController.update);
+orphanagesRouter.patch(
+  '/:id',
+  ensureAuthenticated,
+  celebrate({
+    [Segments.BODY]: {
+      available: Joi.boolean().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  orphanageController.update,
+);
 
 orphanagesRouter.delete(
   '/:id',
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
@@ -16,6 +17,8 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
